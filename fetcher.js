5cac1df0766f5ed4ae02c7838eece4654dd8244b
dganai <dinaganai@gmail.com>
2022-01-19 22:51:36 +0000

    eslint

--- fetcher.js
+++ fetcher.js
@@ -1,23 +1,23 @@
 const fs = require('fs');
 
-const args = process.argv.slice(2)
-const url = args[0]
-const localPath = args[1]
+const args = process.argv.slice(2);
+const url = args[0];
+const localPath = args[1];
 
 
 
-const request = require('request'); // http request and wait for response 
+const request = require('request'); // http request and wait for response
 
 
- request(url, (error, response, body) => {
-    fs.writeFile(localPath, body, (error) => {
+request(url, (error, response, body) => {
+  fs.writeFile(localPath, body, (error) => {
     if (error) {
       console.log(error);
       return;
-    } 
+    }
     console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`);
-    });
-  })
+  });
+});
   
 
 
